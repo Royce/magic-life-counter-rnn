84bf74019e165ec2a30d8cd776e1ea0c058e2bd8
Royce Townsend <git@roycetownsend.com>
2019-07-19 01:18:53 +0800

    Redux: Basic example.

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
+import { connect } from "react-redux";
 
-export default function App() {
+function App({ counter, increment } = props) {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableHighlight onPress={increment}>
+        <Text>Count: {counter}</Text>
+      </TouchableHighlight>
     </View>
   );
 }
 
+function mapStateToProps(state) {
+  return { counter: state.counter };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { increment: () => dispatch({ type: "increment" }) };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+import React from "react";
 import { Navigation } from "react-native-navigation";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./App";
 
-Navigation.registerComponent(`WelcomeScreen`, () => App);
+const reducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case "increment": {
+      return { ...state, counter: state.counter + 1 };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+const store = createStore(reducer);
+
+function WrappedComponent(Component) {
+  return function inject(props) {
+    const EnhancedComponent = () => (
+      <Provider store={store}>
+        <Component {...props} />
+      </Provider>
+    );
+
+    return <EnhancedComponent />;
+  };
+}
+
+Navigation.registerComponent(
+  `WelcomeScreen`,
+  () => WrappedComponent(App),
+  () => App
+);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({

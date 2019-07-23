565cc62f72b3224f29f82e56b5a07fccd8f62fc2
Royce Townsend <git@roycetownsend.com>
2019-07-24 00:37:06 +0800

    Counter text with outline and inversion.

---
+++ src/components/OutlineText.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Text } from "react-native";
+
+export function OutlineText({ text, style, outlineWidth, outlineColor }) {
+  var textIndex = 0;
+  const getStyle = function(x, y) {
+    return {
+      ...style,
+      position: textIndex++ === 1 ? "relative" : "absolute",
+      textShadowColor: outlineColor,
+      textShadowRadius: outlineWidth,
+      textShadowOffset: { width: outlineWidth * x, height: outlineWidth * y }
+    };
+  };
+
+  return (
+    <>
+      <Text style={getStyle(1.0, 0.0)}>{text}</Text>
+      <Text style={getStyle(0.8, 0.8)}>{text}</Text>
+      <Text style={getStyle(0.0, 1.0)}>{text}</Text>
+      <Text style={getStyle(-0.8, 0.8)}>{text}</Text>
+      <Text style={getStyle(-1.0, 0.0)}>{text}</Text>
+      <Text style={getStyle(-0.8, -0.8)}>{text}</Text>
+      <Text style={getStyle(0.0, -1.0)}>{text}</Text>
+      <Text style={getStyle(0.8, -0.8)}>{text}</Text>
+    </>
+  );
+}
--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -3,14 +3,41 @@ import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
 import { connect } from "react-redux";
 
 import { increment } from "../counter/actions";
+import { OutlineText } from "../components";
+
+function CounterTile({ current, color, invert = false }) {
+  return (
+    <View
+      style={[
+        {
+          flex: 1,
+          backgroundColor: color,
+          alignSelf: "stretch",
+          alignItems: "center",
+          justifyContent: "center"
+        },
+        invert ? { transform: [{ rotateX: "180deg" }] } : null
+      ]}
+    >
+      <OutlineText
+        text={current}
+        style={{ color: "white", fontSize: 150, fontWeight: "700" }}
+        outlineColor={"black"}
+        outlineWidth={1}
+      />
+    </View>
+  );
+}
 
 function CounterScreen({ counter, increment } = props) {
   return (
     <View style={styles.container}>
+      <CounterTile color={"red"} current={11} invert={true} />
       <Text>Open up App.js to start working on your app!</Text>
       <TouchableHighlight onPress={increment}>
         <Text>Count: {counter}</Text>
       </TouchableHighlight>
+      <CounterTile color={"blue"} current={99} />
     </View>
   );
 }
